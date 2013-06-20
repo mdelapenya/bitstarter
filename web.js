70cfe22aa4227033708f01b6b325cfe917335512
Ubuntu <ubuntu@ip-10-196-129-57.ec2.internal>
2013-06-20 17:32:18 +0000

    Modify hello world message

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('<h1>Hello World!</h1><p>This is my first Node.js application written for Stanford Startup Engineering course</p>');
 });
 
 var port = process.env.PORT || 5000;
