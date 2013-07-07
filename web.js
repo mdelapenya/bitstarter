c8934ca933db4ab99e96527bf290e2b084f71ff6
Manuel de la Peña <manuel.delapenya@liferay.com>
2013-07-07 11:40:59 +0200

    Change hello world example using an index.html file

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello World!</h1><p>This is my first Node.js application written for Stanford Startup Engineering course</p>');
+	var options = {
+		encoding: "utf-8"
+	};
+
+	var buffer = fs.readFileSync("index.html", options);
+
+	response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
